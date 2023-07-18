0bc379308195d2c6f169b5777d315d03e57f7a0b
indigorose <indigorose049@gmail.com>
2023-07-18 21:32:49 +0100

    Add express and server tests

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # TV Guide Reminder App
 
 A helpful guide to remind when shows are available.
+
+### Steps
+
+#### 1 - Node and express set up testing
+
+Using node and express to create the user api and fetch details from the movie api.
+
+#### 2 - Search the movie database for any tv or show.
+
+search by movie, tv show or actor. https://api.themoviedb.org/3/search/multi
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('May the server be with you');
+// console.log('May the server be with you');
+
+// Importing packages
+const express = require('express');
+const app = express();
+const hello = require('./routes/hello');
+
+// Middlewares?
+app.use(express.json());
+
+// adding routes
+app.use('/hello', hello);
+
+// port
+const port = process.env.PORT || 5500;
+app.listen(port, () => console.log(`Listening on Port: ${port}`));
