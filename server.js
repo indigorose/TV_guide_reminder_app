20e0532d598e978c246e04b50087481c8d0a0e36
indigorose <indigorose049@gmail.com>
2023-07-19 22:19:44 +0100

    Add API and authentication testing

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // console.log('May the server be with you');
 
 // Importing packages
+
 const express = require('express');
+require('dotenv').config();
 const app = express();
+const fetch = (...args) =>
+	import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const API_KEY = process.env.MOVIEDB_API_KEY;
+
 const hello = require('./routes/hello');
 
 // Middlewares?
@@ -10,6 +16,15 @@ app.use(express.json());
 
 // adding routes
 app.use('/hello', hello);
+
+// Fetching API data
+
+const url = 'https://api.themoviedb.org/3/authentication';
+
+fetch(url, options)
+	.then((res) => res.json())
+	.then((json) => console.log(json))
+	.catch((err) => console.error('error:' + err));
 
 // port
 const port = process.env.PORT || 5500;
