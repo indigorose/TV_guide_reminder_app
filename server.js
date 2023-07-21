9beebbbd310ecac11045ed06ffec52bdbe027cd1
indigorose <indigorose049@gmail.com>
2023-07-21 21:53:27 +0100

    API testing

--- server.js
+++ server.js
@@ -3,23 +3,27 @@
 // Importing packages
 
 const express = require('express');
-require('dotenv').config();
 const app = express();
 const fetch = (...args) =>
 	import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const dotenv = require('dotenv').config();
+
 const API_KEY = process.env.MOVIEDB_API_KEY;
+const API_TOKEN = process.env.MOVIEDB_API_TOKEN;
 
 const hello = require('./routes/hello');
 
-// Middlewares?
-app.use(express.json());
-
-// adding routes
-app.use('/hello', hello);
-
 // Fetching API data
 
-const url = 'https://api.themoviedb.org/3/authentication';
+const url = 'https://api.themoviedb.org/3/search/multi?query=thor';
+
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		Authorization: `Bearer ${API_TOKEN}`,
+	},
+};
 
 fetch(url, options)
 	.then((res) => res.json())
