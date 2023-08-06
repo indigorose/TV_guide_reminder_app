101383ffbc26fcbbb5ca78a9aa4ad919d606089e
indigorose <indigorose049@gmail.com>
2023-08-06 16:18:50 +0100

    Add Bootstrap styling

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const dotenv = require('dotenv').config();
 const fetch = (...args) =>
 	import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const API_KEY = process.env.MOVIEDB_API_KEY;
+// API Tokens
 const API_TOKEN = process.env.MOVIEDB_API_TOKEN;
 
+// DOM and Styling
 app.set('view engine', 'ejs');
 
+// ROUTING - For Later
 // const hello = require('./routes/hello');
 
 // Fetching API data
-
-const url = 'https://api.themoviedb.org/3/search/multi?query=thor';
 
 const options = {
 	method: 'GET',
@@ -34,11 +34,8 @@ app.get('/', (req, res) => {
 app.get('/search', async (req, res) => {
 	try {
 		const query = req.query.q; // Get the search query from the URL parameter
-
-		const response = await fetch(
-			`https://api.themoviedb.org/3/search/multi?query=${query}`,
-			options
-		);
+		const url = `https://api.themoviedb.org/3/search/multi?query=${query}`;
+		const response = await fetch(url, options);
 		const data = await response.json();
 		// console.log(data);
 
