d85a8bffbbe393a80e30f7d8a2bdb749baa68221
indigorose <indigorose049@gmail.com>
2023-11-17 18:36:50 +0000

    Update MongoDB connection and post functionality

---
+++ public/main.js
@@ -0,0 +1,4 @@
+const addList = document.querySelector('#add-list');
+const deleteButton = document.querySelector('#delete-media');
+
+addList.addEventListener('click', (_) => {});
--- server.js
+++ server.js
@@ -1,23 +1,63 @@
 // console.log('May the server be with you');
 // To start the server - npm run dev
-// web page is http://127.0.0.1:5500/
+// The web page is http://127.0.0.1:3000/
 
 // Importing packages
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const fetch = (...args) =>
 	import('node-fetch').then(({ default: fetch }) => fetch(...args));
+// const MongoClient = require('mongodb').MongoClient;
+// require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // API Tokens
 const API_TOKEN = process.env.MOVIEDB_API_TOKEN;
+
+// MongoDB Connection
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-crud.inopdty.mongodb.net/?retryWrites=true&w=majority`;
+// Connecting to MongoDB
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(connectionString, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // DOM and Styling
 app.set('view engine', 'ejs');
 
 // ROUTING - For Later
 // const hello = require('./routes/hello');
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+	(client) => {
+		console.log('Connected to the database.');
+		const db = client.db('media-api-list');
+	}
+);
 
 // Fetching API data
 
@@ -39,7 +79,7 @@ app.get('/search', async (req, res) => {
 		const url = `https://api.themoviedb.org/3/search/multi?query=${query}`;
 		const response = await fetch(url, options);
 		const data = await response.json();
-		// console.log(data);
+		console.log(data);
 
 		res.render('search-results', { query, results: data.results });
 	} catch (error) {
