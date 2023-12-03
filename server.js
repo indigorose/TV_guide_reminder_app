25724869d6dd07119bfd885d81e6caa7a37b5602
indigorose <indigorose049@gmail.com>
2023-12-03 14:55:47 +0000

    Add styling tests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // console.log('May the server be with you');
+// Sign into MongoDB before starting so it connects to the server
 // To start the server - npm run dev
+
 // The web page is http://127.0.0.1:3000/
 
 // Importing packages
@@ -26,73 +28,71 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
-	(client) => {
-		console.log('Connected to the Database');
-		const db = client.db('mediaApiList');
-		const mediaCollection = db.collection('mediaTitles');
-		const options = {
-			method: 'GET',
-			headers: {
-				accept: 'application/json',
-				Authorization: `Bearer ${API_TOKEN}`,
-			},
-		};
+MongoClient.connect(connectionString).then((client) => {
+	console.log('Connected to the Database');
+	const db = client.db('mediaApiList');
+	const mediaCollection = db.collection('mediaTitles');
+	const options = {
+		method: 'GET',
+		headers: {
+			accept: 'application/json',
+			Authorization: `Bearer ${API_TOKEN}`,
+		},
+	};
 
-		app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.urlencoded({ extended: true }));
 
-		app.get('/', (req, res) => {
-			const list = db
-				.collection('mediaTitles')
-				.find()
-				.toArray()
-				.then((results) => {
-					res.render('index.ejs', { list: results });
+	app.get('/', (req, res) => {
+		const list = db
+			.collection('mediaTitles')
+			.find()
+			.toArray()
+			.then((results) => {
+				res.render('index.ejs', { list: results });
+			});
+	});
+	app.get('/search', async (req, res) => {
+		try {
+			const query = req.query.q; // Get the search query from the URL parameter
+			const url = `https://api.themoviedb.org/3/search/multi?query=${query}`;
+			const response = await fetch(url, options);
+			const data = await response.json();
+			console.log(data);
+
+			res.render('search-results', { query, results: data.results });
+		} catch (error) {
+			console.error('Error fetching data:', error);
+			res.status(500).json({ error: 'Error fetching data' });
+		}
+	});
+
+	app.post('/', async (req, res) => {
+		try {
+			const { title, release_date, overview } = req.body;
+			if (title && title.trim() !== '') {
+				await mediaCollection.insertOne({
+					title,
+					releaseDate: release_date,
+					overview,
 				});
-		});
-		app.get('/search', async (req, res) => {
-			try {
-				const query = req.query.q; // Get the search query from the URL parameter
-				const url = `https://api.themoviedb.org/3/search/multi?query=${query}`;
-				const response = await fetch(url, options);
-				const data = await response.json();
-				console.log(data);
-
-				res.render('search-results', { query, results: data.results });
-			} catch (error) {
-				console.error('Error fetching data:', error);
-				res.status(500).json({ error: 'Error fetching data' });
-			}
-		});
-
-		app.post('/', async (req, res) => {
-			try {
-				const { title, release_date, overview } = req.body;
-				if (title && title.trim() !== '') {
-					await mediaCollection.insertOne({
-						title,
-						releaseDate: release_date,
-						overview,
+				db.collection('mediaTitles')
+					.find()
+					.toArray()
+					.then((results) => {
+						res.render('list.ejs', { mediaTitles: results });
 					});
-					db.collection('mediaTitles')
-						.find()
-						.toArray()
-						.then((results) => {
-							res.render('list.ejs', { mediaTitles: results });
-						});
-					console.log('Movie title has been added');
-				} else {
-					res.status(400).send('Movie title cannot be empty.');
-				}
-			} catch (err) {
-				console.error(err);
-				res.status(500).send('Error adding movie to list');
+				console.log('Movie title has been added');
+			} else {
+				res.status(400).send('Movie title cannot be empty.');
 			}
-		});
-		const port = process.env.PORT || 3000;
-		app.listen(port, () => console.log(`Listening on Port: ${port}`));
-	}
-);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Error adding movie to list');
+		}
+	});
+	const port = process.env.PORT || 3000;
+	app.listen(port, () => console.log(`Listening on Port: ${port}`));
+});
 
 // ROUTING - For Later
 // const hello = require('./routes/hello');
