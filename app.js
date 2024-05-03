b180a130d0d90aade2ed2d7d8bb394d1ba34b987
indigorose <indigorose049@gmail.com>
2024-05-03 18:23:20 +0100

    JWT token testing

--- app.js
+++ app.js
@@ -12,60 +12,15 @@
 // The web page is http://127.0.0.1:3000/
 
 // Importing packages
-require('dotenv').config();
+const express = require('express');
+const dotenv = require('dotenv');
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var logger = require('morgan');
-var passport = require('passport');
-var session = require('express-session');
+dotenv.config({ path: './config.env' });
 
-var SQLiteStore = require('connect-sqlite3')(session);
+const PORT = process.env.PORT;
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-var app = express();
+const app = express();
 
-app.locals.pluralize = require('pluralize');
+app.get('/', (req, res) => res.send('Server is ready.'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-	session({
-		secret: 'keyboard cat',
-		resave: false,
-		saveUninitialized: false,
-		store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' }),
-	})
-);
-app.use(passport.authenticate('session'));
-app.use('/', indexRouter);
-app.use('/', authRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
-
-module.exports = app;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
