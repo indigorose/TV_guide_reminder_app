0064dc8c3476a002271ec29679b45818517298a0
indigorose <indigorose049@gmail.com>
2024-05-06 20:05:29 +0100

    Debugging and completing backend authentication

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 dotenv.config({ path: './config.env' });
 connectDB();
@@ -25,6 +26,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready.'));
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,19 @@ import generateToken from '../utils/generateToken.js';
 // route    POST /api/users/auth
 // @access  Public - non protected route
 const authUser = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Auth User' });
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (user && (await user.matchPassword(password))) {
+		generateToken(res, user._id);
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+	} else {
+		res.status(401);
+		throw new Error('Invalid email or password');
+	}
 });
 // @desc    Register a new user
 // route    POST /api/users
@@ -39,21 +51,56 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Logout User' });
+	res.cookie('jwt', '', {
+		httpOnly: true,
+		expires: new Date(0),
+	});
+	res.status(200).json({ message: 'User has logged out.' });
 });
 
 // @desc    Get user profile
-// route    GET /api/users/profile
+// @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'User profile' });
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+	} else {
+		res.status(404);
+		throw new Error('User not found');
+	}
 });
 
 // @desc    Update user profile
-// route    PUT /api/users/profile
+// @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Update user profile' });
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+		});
+	} else {
+		res.status(404);
+		throw new Error('User not found');
+	}
 });
 
 export {
